c4cd297ed7b119b5771bfd4b1ab69e5ed3ddf90d
Avigail Yazdi <ayazdi@g.jct.ac.il>
2026-04-13 13:55:39 +0300

    admin/user

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true, match: [/^\S+@\S+\.\S+$/, "Invalid email address"] },
     passwordHash: { type: String, required: true, select: false, minlength: 6 },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
+    role: { type: String, enum: ["משתמש", "מנהל"], default: "משתמש" },
     favorites: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Recipe"
